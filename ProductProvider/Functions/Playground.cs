a32675490c062dab2cb44ed306c03dddd76a9655
Thanakorn Saengprachum <ThanaSaengprachum@hotmail.com>
2024-11-04 11:38:29 +0100

    Added logging to playground function

--- ProductProvider/Functions/Playground.cs
+++ ProductProvider/Functions/Playground.cs
@@ -1,47 +1,60 @@
-using Castle.Components.DictionaryAdapter.Xml;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.Cosmos.Scripts;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
 
-namespace ProductProvider.Functions;
-
-public class Playground
+namespace ProductProvider.Functions
 {
-    private readonly ILogger<Playground> _logger;
-
-    public Playground(ILogger<Playground> logger)
+    public class Playground
     {
-        _logger = logger;
-    }
+        private readonly ILogger<Playground> _logger;
 
-    [Function("Playground")]
-    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = "graphql")] HttpRequestData req)
-    {
-        var response = req.CreateResponse();
-        response.Headers.Add("Content-type", "text/html; charset=utf-8");
-        await response.WriteStringAsync(Playgroundpage());
-        return response;
+        public Playground(ILogger<Playground> logger)
+        {
+            _logger = logger;
+        }
 
-    }
+        [Function("Playground")]
+        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "graphql")] HttpRequestData req)
+        {
+            _logger.LogInformation("Starting execution of 'Playground' function with request method: {Method}", req.Method);
 
-    private string Playgroundpage()
-    {
+            try
+            {
+                _logger.LogInformation("Creating response with GraphQL Playground page.");
+                var response = req.CreateResponse();
+                response.Headers.Add("Content-type", "text/html; charset=utf-8");
 
+                await response.WriteStringAsync(Playgroundpage());
 
-        return  @"
+                _logger.LogInformation("Response created and returned successfully.");
+                return response;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while processing the 'Playground' function request.");
+                var errorResponse = req.CreateResponse(System.Net.HttpStatusCode.InternalServerError);
+                await errorResponse.WriteStringAsync("An internal server error occurred.");
+                return errorResponse;
+            }
+        }
+
+        private string Playgroundpage()
+        {
+            _logger.LogDebug("Generating HTML content for the GraphQL Playground page.");
+
+            return @"
                 <!DOCTYPE html>
                 <html>
                 <head>
                 <title>HotChocolate GraphQL Playground</title>
-                <link rel="" stylesheet"" href=""https://cdn.jsdelivr.net/npm/graphql-playground-react/build/static/css/index.css"" />
-                <link rel=""shortcut icon"" href = ""https://cdn.jsdelivr.net/npm/graphql-playground-react/build/favicon.png"" />
-                <script src = ""https://cdn.jsdelivr.net/npm/graphql-playground-react/build/static/js/middleware.js""></script>
+                <link rel=""stylesheet"" href=""https://cdn.jsdelivr.net/npm/graphql-playground-react/build/static/css/index.css"" />
+                <link rel=""shortcut icon"" href=""https://cdn.jsdelivr.net/npm/graphql-playground-react/build/favicon.png"" />
+                <script src=""https://cdn.jsdelivr.net/npm/graphql-playground-react/build/static/js/middleware.js""></script>
                 </head> 
                 <body>
-                    <div id =""root""></div>
+                    <div id=""root""></div>
                     <script>
                     window.addEventListener('load', function(event) {
                             GraphQLPlayground.init(document.getElementById('root'), {
@@ -51,6 +64,6 @@ public class Playground
                     </script>
                 </body>
                 </html>";
-
+        }
     }
 }
